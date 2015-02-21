e85f8677596cd875a6edd3dc5aba146e4ae33764
yanzhe19 <yanzhe19@gmail.com>
2015-02-20 19:09:36 -0500

    home page finished

--- COMP308-jQueryMobile/Scripts/_references.js
+++ COMP308-jQueryMobile/Scripts/_references.js
@@ -2,3 +2,7 @@
 /// <reference path="jquery-1.8.0.js" />
 /// <reference path="jquery.mobile-1.4.5.js" />
 /// <reference path="mobile.js" />
+/// <reference path="../engine1/jquery.js" />
+/// <reference path="../engine1/script.js" />
+/// <reference path="../engine1/wowslider.js" />
+/// <reference path="../engine1/wowslider.mod.js" />
